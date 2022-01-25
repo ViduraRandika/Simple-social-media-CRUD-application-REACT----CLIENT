fcb8b6a8cd665e30c8ca5f6e0f2193a683fd62c0
Vidura Randika <cst17047@std.uwu.ac.lk>
2022-01-25 11:47:31 +0530

    post and form components created

---
+++ src/components/Form/Form.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Form = () => {
+	return(
+		<h1>FORM COMPONENT</h1>
+	);
+};
+
+export default Form;
---
+++ src/components/Posts/Post/Post.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Post = () => {
+	return(
+		<h1>SINGLE POST COMPONENT</h1>
+	);
+};
+
+export default Post;
